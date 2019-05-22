e55569946966fbed0dde722583f9aaa007ea5b77
524300045@qq.com <524300045@qq.com>
2019-05-22 23:03:13 +0800

    配置文件修改

--- mall-portal/src/main/java/com/macro/mall/portal/domain/QueueEnum.java
+++ mall-portal/src/main/java/com/macro/mall/portal/domain/QueueEnum.java
@@ -30,6 +30,18 @@ public enum QueueEnum {
      */
     private String routeKey;
 
+    public String getExchange() {
+        return exchange;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRouteKey() {
+        return routeKey;
+    }
+
     QueueEnum(String exchange, String name, String routeKey) {
         this.exchange = exchange;
         this.name = name;
